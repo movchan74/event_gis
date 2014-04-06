d358939850cd98fec3d04bc11f74039a130d0445
Vsevolod Radchenko <kazarey@gmail.com>
2014-04-06 19:39:55 +0600

    [#67759774] Реализован rest-запрос для получения списка типов событий. Изменена логика rest-запроса для фильтрации событий

--- event_gis/events/urls.py
+++ event_gis/events/urls.py
@@ -2,9 +2,10 @@
 from django.conf.urls import patterns, include, url
 
 from views import render_main_page
-from views import FilterEvents
+from views import FilterEvents, EventTypes
 
 urlpatterns = patterns('',
     url(r'^$', render_main_page),
     url(r'^filter_events$', FilterEvents.as_view()),
+    url(r'^event_types$', EventTypes.as_view()),
 )
--- event_gis/events/views.py
+++ event_gis/events/views.py
@@ -11,24 +11,43 @@ def render_main_page(request):
 
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.renderers import UnicodeJSONRenderer
-from events.models import Event
-from events.serializers import EventSerializer
+from events.models import Event, EventType
+from events.serializers import EventSerializer, EventTypeSerializer
 from datetime import datetime
 
-# http://127.0.0.1:8000/filter_events?type=1+3&start_time=2013-10-25+13-30&end_time=2013-10-26+10-00
+# http://127.0.0.1:8000/filter_events?type=1+2+3&start_time=2000-1-25+13-30&end_time=2015-10-26+13-30
 class FilterEvents(ListAPIView):
     renderer_classes = (UnicodeJSONRenderer,)
     serializer_class = EventSerializer
 
     def get_queryset(self):
-        types = self.request.QUERY_PARAMS.get('type').split()
-        types = map( int, types )
+        query_objects = Event.objects.all()
 
-        start_time_str = self.request.QUERY_PARAMS.get('start_time')
-        start_time = datetime.strptime( start_time_str, "%Y-%m-%d %H-%M" )
+        if 'type' in self.request.QUERY_PARAMS:
+            types = self.request.QUERY_PARAMS.get('type').split()
+            types = map( int, types )
+            query_objects = query_objects.filter( event_type__in = types )
 
-        end_time_str = self.request.QUERY_PARAMS.get('end_time')
-        end_time = datetime.strptime( end_time_str, "%Y-%m-%d %H-%M" )
+        if 'start_time' in self.request.QUERY_PARAMS:
+            start_time_str = self.request.QUERY_PARAMS.get('start_time')
+            start_time = datetime.strptime( start_time_str, "%Y-%m-%d %H-%M" )
+            query_objects = query_objects.filter( start_time__gt = start_time )
 
-        return Event.objects.filter( event_type__in = types ).filter( start_time__gt = start_time).filter( end_time__lt = end_time )
+        if 'end_time' in self.request.QUERY_PARAMS:
+            end_time_str = self.request.QUERY_PARAMS.get('end_time')
+            end_time = datetime.strptime( end_time_str, "%Y-%m-%d %H-%M" )
+            query_objects = query_objects.filter( end_time__lt = end_time )
+
+        return query_objects
+
+
+# http://127.0.0.1:8000/event_types
+class EventTypes(ListAPIView):
+    renderer_classes = (UnicodeJSONRenderer,)
+    serializer_class = EventTypeSerializer
+
+    def get_queryset(self):
+        return EventType.objects.all()
+
+
 
